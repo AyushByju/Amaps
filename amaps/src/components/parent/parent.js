7c65479f68dbd6d676c2df10c8bc79fae1ccb207
AyushByju <87507065+AyushByju@users.noreply.github.com>
2023-08-08 12:32:38 -0400

    changed parent.js

--- amaps/src/components/parent/parent.js
+++ amaps/src/components/parent/parent.js
@@ -3,41 +3,60 @@ import React, { useState, useEffect } from 'react';
 import SearchBox from '../search/search.js';
 import Map from '../map/map.js';
 import Info from '../information/info.js';
-import CheckboxFilter from '../filter/filter.js'; // Import the new component
+import CheckboxFilter from '../filter/filter.js';
 
 const ParentComponent = () => {
   const [mapData, setMapData] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
-  const [checkedCategories, setCheckedCategories] = useState([]);
-  const [uniqueCategories, setUniqueCategories] = useState([]);
+  const [checkedCFINCategories, setCheckedCFINCategories] = useState([]);
+  const [uniqueCFINCategories, setUniqueCFINCategories] = useState([]);
+  const [checkedProductServices, setCheckedProductServices] = useState([]);
+  const [uniqueProductServices, setUniqueProductServices] = useState([]);
 
   useEffect(() => {
-    // Extract unique CFIN_Category values from mapData
-    const categories = [...new Set(mapData.map(item => item.CFIN_Category))];
-    setUniqueCategories(categories);
-    setCheckedCategories(categories);  // Set all categories as initially checked
+    // Extract unique CFIN_Category and Product_or_Service values from mapData
+    const CFINCategories = [...new Set(mapData.map(item => item.CFIN_Category))];
+    const productServices = [...new Set(mapData.map(item => item.Product_or_Service))];
+    setUniqueCFINCategories(CFINCategories);
+    setCheckedCFINCategories(CFINCategories);  // Set all CFIN categories as initially checked
+    setUniqueProductServices(productServices);
+    setCheckedProductServices(productServices);  // Set all product services as initially checked
   }, [mapData]);
+  
 
   const handleDataFetch = (data, query) => {
     setMapData(data);
     setSearchQuery(query);
   };
 
-  // Toggle a category's checked state
-  const handleCategoryToggle = (category) => {
-    setCheckedCategories(prevState => {
+  // Toggle a CFIN category's checked state
+  const handleCFINCategoryToggle = (category) => {
+    setCheckedCFINCategories(prevState => {
       if (prevState.includes(category)) {
-        // Remove category from checkedCategories
+        // Remove category from checkedCFINCategories
         return prevState.filter(c => c !== category);
       } else {
-        // Add category to checkedCategories
+        // Add category to checkedCFINCategories
         return [...prevState, category];
       }
     });
   };
 
-  // Filter mapData based on checkedCategories
-  const filteredData = mapData.filter(item => checkedCategories.includes(item.CFIN_Category));
+  // Toggle a product service's checked state
+  const handleProductServiceToggle = (service) => {
+    setCheckedProductServices(prevState => {
+      if (prevState.includes(service)) {
+        // Remove service from checkedProductServices
+        return prevState.filter(s => s !== service);
+      } else {
+        // Add service to checkedProductServices
+        return [...prevState, service];
+      }
+    });
+  };
+
+  // Filter mapData based on checkedCFINCategories and checkedProductServices
+  const filteredData = mapData.filter(item => checkedCFINCategories.includes(item.CFIN_Category) && checkedProductServices.includes(item.Product_or_Service));
 
   return (
     <div>
@@ -45,10 +64,19 @@ const ParentComponent = () => {
       <SearchBox onFetchData={handleDataFetch} />
       <Info searchQuery={searchQuery} />
       <CheckboxFilter
-        categories={uniqueCategories}
-        checkedCategories={checkedCategories}
-        onCategoryToggle={handleCategoryToggle}
+        title="CFIN Category"
+        categories={uniqueCFINCategories}
+        checkedCategories={checkedCFINCategories}
+        onCategoryToggle={handleCFINCategoryToggle}
       />
+
+      <CheckboxFilter
+        title="Product or Service"
+        categories={uniqueProductServices}
+        checkedCategories={checkedProductServices}
+        onCategoryToggle={handleProductServiceToggle}
+      />
+
     </div>
   );
 };
